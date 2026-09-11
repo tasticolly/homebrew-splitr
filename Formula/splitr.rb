799ef0e90d571dc3ae4975df2d263dead272fe2d
Крехов Николай Александрович <nkrekhov@mts.ru>
2026-09-11 13:52:08 +0300

    splitr v0.5.3

--- Formula/splitr.rb
+++ Formula/splitr.rb
@@ -1,8 +1,8 @@
 class Splitr < Formula
   desc "Sshuttle tunnel manager for macOS with a pf-based kill switch"
   homepage "https://github.com/tasticolly/splitr"
-  url "https://github.com/tasticolly/splitr/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "b947509c943b91135969d55c106ac2f5b42cea166d2dbb2f744fb718ed809ff3"
+  url "https://github.com/tasticolly/splitr/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "3dfb010deb4847bb5669e555334bc045b2803dd0981de691ad863fea674acc75"
   license "MIT"
   head "https://github.com/tasticolly/splitr.git", branch: "main"
 
